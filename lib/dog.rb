356ae150bfcde50434868ce1ec85422d9cc8ac06
Johannes Hupfeld <j.hupfeld@gmail.com>
2019-10-20 03:14:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,18 @@
-# Your code goes here!
-class Dog
-  fido = Dog.new
-fido.name = "Fido"
- 
-fido.name
-# => "Fido"
- 
- def bark
-   puts "woof!"
- end
 
-end
-# => nil
+class Dog
+  def name=(dogs_name)
+    @name = dogs_name
+  end 
+
+  def name
+    @name
+  end 
+  
+end 
+
+class Dog
+  def bark
+    puts "woof!"
+  end 
+  
+end 
